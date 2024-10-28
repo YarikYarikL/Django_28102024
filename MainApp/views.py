f87827c35d72a0dc820334cbf05f3bc72abb49d6
Lazarev Yaroslav <yarik@mail.ru>
2024-10-28 16:28:04 +0300

    M1:2 done

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+}
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
@@ -9,4 +17,12 @@ def home(request):
     """
     return HttpResponse(text)
 
-
+def about (request):
+    text = f"""
+    Имя: <b>{author["Имя"]}</b><br>
+    Отчество: <b>{author["Отчество"]}</b><br>
+    Фамилия: <b>{author["Фамилия"]}</b><br>
+    телефон: <b>{author["телефон"]}</b><br>
+    email: <b>{author["email"]}</b><br>
+    """
+    return HttpResponse(text)
